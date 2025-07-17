461bfd85c874aa21b92695fa6ea515239399923c
drdilyor <drdilyor@outlook.com>
2025-07-17 10:30:53 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  # This is a template created by `hix init`
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (haskellPackages.ghcWithPackages (pkgs: with pkgs; [ cabal-install ]))
+          ghcid
+          haskell-language-server
+        ];
+      };
+    };
+}
