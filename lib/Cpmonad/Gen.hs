354ee0f9add41940af627dc516bd8b29d17dbcb2
drdilyor <drdilyor@outlook.com>
2025-08-16 14:07:44 +0200

    refactor: switch to Gen without ST. Gen with ST is now special case

--- lib/Cpmonad/Gen.hs
+++ lib/Cpmonad/Gen.hs
@@ -1,8 +1,11 @@
 module Cpmonad.Gen(
   Gen,
-  lift,
-  state,
+  GenST,
   runGen,
+  runGenST,
+  liftg,
+  runIOGen,
+
   genr,
   genri,
   Indexable(..),
@@ -12,29 +15,40 @@ module Cpmonad.Gen(
   shuffle,
   genperm,
   gendistinct,
+  lift,
+  state,
 ) where
 
-import Control.Monad
 import Control.Monad.ST
-import Control.Monad.State.Strict (StateT (..), state, lift)
+import Control.Monad.State.Strict (State, StateT, state, lift, runState, runStateT)
 import Data.List
 import Data.Set qualified as Set
-import Data.Vector (Vector)
+import Data.Vector (Vector, (!))
 import Data.Vector qualified as V
 import Data.Vector.Mutable qualified as VM
 import Data.Vector.Strict qualified as VS
 import Data.Vector.Algorithms.Merge qualified as VA
-import System.Random (uniformR, StdGen)
+import System.Random (uniformR, StdGen, newStdGen)
 
-type Gen s = StateT StdGen (ST s)
+type Gen = State StdGen
+type GenST s = StateT StdGen (ST s)
 
-runGen :: (forall s. Gen s a) -> StdGen -> (a, StdGen)
-runGen g s = runST $ runStateT g s
+runGen :: Gen a -> StdGen -> (a, StdGen)
+runGen = runState
 
-genr :: Int -> Int -> Gen s Int
+runGenST :: (forall s. GenST s a) -> Gen a
+runGenST g = state \s -> runST $ runStateT g s
+
+liftg :: Gen a -> GenST s a
+liftg = state . runState
+
+runIOGen :: Gen a -> IO a
+runIOGen g = fst . runGen g <$> newStdGen
+
+genr :: Int -> Int -> Gen Int
 genr a b = state $ uniformR (a, b-1)
 
-genri :: Int -> Int -> Gen s Int
+genri :: Int -> Int -> Gen Int
 genri a b = genr a (b-1)
 
 class Indexable c where
@@ -53,52 +67,55 @@ instance Indexable VS.Vector where
   index = (VS.!)
   size = VS.length
 
-choose :: Indexable c => c a -> Gen s a
+choose :: Indexable c => c a -> Gen a
 choose xs = let n = size xs in index xs <$> genr 0 n
 
-genuntil :: (a -> Bool) -> Gen s a -> Gen s a
+genuntil :: (a -> Bool) -> Gen a -> Gen a
 genuntil p g = do
   x <- g
   if p x
     then pure x
     else genuntil p g
 
-genpair :: (Int -> Int -> Bool) -> Gen s Int -> Gen s (Int, Int)
+genpair :: (Int -> Int -> Bool) -> Gen Int -> Gen (Int, Int)
 genpair f g = genuntil (uncurry f) $ liftA2 (,) g g
 
-distribute :: Int -> Int -> Int -> Gen s (Vector Int)
+distribute :: Int -> Int -> Int -> Gen (Vector Int)
 distribute _ _ low | low < 0 = error "low must be non-negative"
 distribute _ n _ | n <= 0 = error "n must be positive"
-distribute s n 0 = do
-  v <- VM.replicateM (n - 1) $ genr 0 s
+distribute s n 0 = runGenST do
+  v <- VM.generateM (n + 1) \i ->
+    if
+      | i == 0 -> pure 0
+      | i == n -> pure s
+      | otherwise -> liftg (genr 0 s)
   VA.sort v
-  v' <- V.unsafeFreeze v
-  let delimeters = V.singleton 0 <> v' <> V.singleton s
+  delimeters <- V.unsafeFreeze v
   pure $ V.zipWith (-) (V.drop 1 delimeters) delimeters
 distribute s n low = V.map (+ low) <$> distribute (s - low * n) n 0
 
-shuffle' :: VM.STVector s Int -> Gen s (Vector Int)
-shuffle' v = do
-  let n = VM.length v
-  forM_ [0 .. n - 1] \i -> do
-    j <- genr i n
-    x <- VM.read v i
-    VM.write v j x
-  V.unsafeFreeze v
+shuffle :: Vector Int -> Gen (Vector Int)
+shuffle v' = runGenST do
+  let n = V.length v'
+  let go v i
+        | i == n = V.unsafeFreeze v
+        | otherwise = do
+            j <- liftg $ genr i n
+            VM.swap v i j
+            go v (i + 1)
+  v <- V.thaw v'
+  go v 0
 
-shuffle :: Vector Int -> Gen s (Vector Int)
-shuffle v = shuffle' =<< V.thaw v
+genperm :: Int -> Gen (Vector Int)
+genperm n = shuffle $ V.enumFromN 0 n
 
-genperm :: Int -> Gen s (Vector Int)
-genperm n = shuffle' =<< V.unsafeThaw (V.enumFromN 0 n)
-
-gendistinct :: (Ord a) => Int -> Gen s a -> Gen s (Vector a)
-gendistinct n g = VM.new n >>= go 0 Set.empty
-  where
-    go i seen v =
-      if i == n
-        then V.unsafeFreeze v
-        else do
-          x <- genuntil (not . flip Set.member seen) g
-          VM.write v i x
-          go (i + 1) (Set.insert x seen) v
+gendistinct :: (Ord a) => Int -> Gen a -> Gen (Vector a)
+gendistinct n g = runGenST do
+  v <- VM.new n
+  let go i seen v
+        | i == n = V.unsafeFreeze v
+        | otherwise = do
+            x <- liftg $ genuntil (not . flip Set.member seen) g
+            VM.write v i x
+            go (i + 1) (Set.insert x seen) v
+  go 0 Set.empty v
