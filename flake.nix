f804549c9d6b0ed776f027c38422fd657c1aa77f
drdilyor <drdilyor@outlook.com>
2025-11-10 22:34:51 +0100

    fix: now devShell uses cabal2nix as well

--- flake.nix
+++ flake.nix
@@ -4,35 +4,47 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-      "aarch64-darwin"
-    ];
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
-    perSystem = { system, pkgs, ... }:
-      let
-        hpkgs = pkgs.haskell.packages.ghc912;
+      perSystem = { system, pkgs, ... }:
+        let
+          hpkgs = pkgs.haskell.packages.ghc912;
 
-        cpmonad = pkgs.haskell.lib.overrideCabal (hpkgs.callCabal2nix "cpmonad" ./. { }) (old: {
-          doCheck = true;
-          doHaddock = false;
-          enableLibraryProfiling = false;
-          enableExecutableProfiling = false;
-        });
-      in
-      {
-        packages.default = cpmonad;
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.coreutils
-            hpkgs.ghc
-            hpkgs.cabal-install
-            hpkgs.ghcid
-            hpkgs.haskell-language-server
-          ];
+          cpmonad = returnShellEnv:
+            hpkgs.developPackage {
+              root = ./.;
+              inherit returnShellEnv;
+              modifier = drv:
+                let drv' = 
+                  if returnShellEnv then
+                    pkgs.haskell.lib.addBuildTools drv (
+                      with hpkgs;
+                      [
+                        cabal-install
+                        ghcid
+                        haskell-language-server
+                      ]
+                    )
+                  else
+                    drv;
+                in
+                pkgs.haskell.lib.overrideCabal drv' (old: {
+                  doCheck = true;
+                  # didn't work for some reason
+                  doHaddock = returnShellEnv;
+                  enableLibraryProfiling = returnShellEnv;
+                  enableExecutableProfiling = returnShellEnv;
+                });
+            };
+        in
+        {
+          packages.default = cpmonad false;
+          devShells.default = cpmonad true;
         };
-      };
-  };
+    };
 }
