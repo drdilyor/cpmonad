6d6812458927dc67970fa41639e1c21da855b5a8
drdilyor <drdilyor@outlook.com>
2025-12-24 22:46:47 +0100

    update flake.lock and format flake.nix

--- flake.nix
+++ flake.nix
@@ -4,42 +4,40 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
 
-      perSystem = { system, pkgs, ... }:
+      perSystem =
+        { system, pkgs, ... }:
         let
           hpkgs = pkgs.haskell.packages.ghc912;
 
-          cpmonad = returnShellEnv:
+          cpmonad =
+            returnShellEnv:
+
             hpkgs.developPackage {
               root = ./.;
               inherit returnShellEnv;
-              modifier = drv:
-                let drv' = 
-                  if returnShellEnv then
-                    pkgs.haskell.lib.addBuildTools drv (
-                      with hpkgs;
-                      [
-                        cabal-install
-                        ghcid
-                        haskell-language-server
-                      ]
-                    )
-                  else
-                    drv;
-                in
-                pkgs.haskell.lib.overrideCabal drv' (old: {
-                  doCheck = true;
-                  # didn't work for some reason
-                  doHaddock = returnShellEnv;
-                  enableLibraryProfiling = returnShellEnv;
-                  enableExecutableProfiling = returnShellEnv;
-                });
+              modifier = pkgs.haskell.lib.compose.overrideCabal (old: {
+                buildTools =
+                  (old.buildTools or [ ])
+                  ++ pkgs.lib.optionals returnShellEnv [
+                    hpkgs.cabal-install
+                    hpkgs.ghcid
+                    hpkgs.haskell-language-server
+                  ];
+
+                doCheck = true;
+                doHaddock = returnShellEnv;
+                enableLibraryProfiling = returnShellEnv;
+                enableExecutableProfiling = returnShellEnv;
+
+              });
             };
         in
         {
